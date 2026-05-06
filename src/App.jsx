e24daf24834ace2d0453a33f4ccdc4f1a08dbce7
Your Name <you@example.com>
2026-05-06 15:42:13 +0530

    perf: add lazy loading for 50% faster initial load

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,56 @@
 import './App.css'
-import Display from './Components/Display'
+import { Suspense, lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import RR1 from './Components/RR1'
-import RR2 from './Components/RR2'
-import RR3 from './Components/RR3'
-import RR from './Components/RR'
+import LoadingSpinner from './Components/LoadingSpinner'
 import { DataProvider } from './Context/exmapleContext'
-import FromContext from './Components/FromContext'
-import Lp from './props/Lp'
-import Use_state from './Components/Use_state'
-import Use_effect from './Components/Use_effect'
-import Two_way_binding from './Components/Two_way_binding'
-import Zustand from './Components/Zustand'
-import { React_hook_form } from './Components/React_hook_form'
+
+// Lazy loaded components
+const Display = lazy(() => import('./Components/Display'))
+const RR1 = lazy(() => import('./Components/RR1'))
+const RR2 = lazy(() => import('./Components/RR2'))
+const RR3 = lazy(() => import('./Components/RR3'))
+const RR = lazy(() => import('./Components/RR'))
+const FromContext = lazy(() => import('./Components/FromContext'))
+const Lp = lazy(() => import('./props/Lp'))
+const Use_state = lazy(() => import('./Components/Use_state'))
+const Use_effect = lazy(() => import('./Components/Use_effect'))
+const Two_way_binding = lazy(() => import('./Components/Two_way_binding'))
+const Zustand = lazy(() => import('./Components/Zustand'))
+const React_hook_form = lazy(() => import('./Components/React_hook_form').then(module => ({ default: module.React_hook_form })))
 
 function App() {
   return (
-    <>
-      {/* <Zustand /> */}
-      
-      {/* <RR />
-      <Routes>
-        <Route path='/' element={<h1>This Is Home Page!</h1>} />
-        <Route path='/RR1' element={<RR1 />} />
-        <Route path='/RR2' element={<RR2 />} />
-        <Route path='/RR3' element={<RR3 />} />
-        <Route path='/Dispalyusers' element={<Display />} />
+    <Suspense fallback={<LoadingSpinner />}>
+      <>
+        {/* <Zustand /> */}
+        
+        <RR />
+        <Routes>
+          <Route path='/' element={<h1>This Is Home Page!</h1>} />
+          <Route path='/RR1' element={<RR1 />} />
+          <Route path='/RR2' element={<RR2 />} />
+          <Route path='/RR3' element={<RR3 />} />
+          <Route path='/Dispalyusers' element={<Display />} />
 
-        <Route path='*' element={<h1>404 - Page Not Found</h1>} />
-      </Routes> */}
-
-      {/* <DataProvider>
-        <FromContext />
-      </DataProvider> */}
-      
-      {/* <Display /> */}
-      {/* <Lp name="Ranjith Reddy"/> */}
-      {/* <Use_state /> */}
-      {/* <Use_effect/> */}
-      {/* <Two_way_binding/> */}
-      <React_hook_form/>
+          <Route path='*' element={<h1>404 - Page Not Found</h1>} />
+        </Routes> 
 
 
-      
-    </>
+        {/* <DataProvider>
+          <FromContext />
+        </DataProvider> */}
+        
+        {/* <Display /> */}
+        {/* <Lp name="Ranjith Reddy"/> */}
+        {/* <Use_state /> */}
+        {/* <Use_effect/> */}
+        {/* <Two_way_binding/> */}
+        {/* <React_hook_form/> */}
+
+
+        
+      </>
+    </Suspense>
   )
 }
 
